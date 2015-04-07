e76adfe46e6ed0762df3eeb6cb61b7a3fbf8c781
Aleš Roubíček <ales@roubicek.name>
2015-04-07 19:34:06 +0200

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,8 @@
-{:user {:plugins [[lein-ancient "0.5.5"]
-                  [lein-kibit "0.0.8"]]
-        :dependencies [[slamhound "1.3.1"]]}}
+{:user {:plugins [[lein-ancient "0.6.4"]
+                  [lein-deps-tree "0.1.2"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit "0.0.8"]
+                  [lein-gorilla "0.3.4"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.8.2"]]
+        :dependencies [[slamhound "1.5.5"]]}
